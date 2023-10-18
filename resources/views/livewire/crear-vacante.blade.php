21cd725cf3a4920bbdba05084b105ab45e8159fc
Javier Dominguez Galindo <joshjavy@gmail.com>
2023-10-17 19:33:47 -0600

    198. Conectar el Formulario a Livewire

--- resources/views/livewire/crear-vacante.blade.php
+++ resources/views/livewire/crear-vacante.blade.php
@@ -5,7 +5,8 @@
         <x-input-label for="titulo" :value="__('Titulo Vacante')" />
         <x-text-input id="titulo"
             class="block mt-1 w-full"
-            type="text" name="titulo"
+            type="text"
+            wire:model="titulo"
             :value="old('titulo')"
             placeholder="Titulo Vacante" />
         <x-input-error :messages="$errors->get('titulo')" class="mt-2" />
@@ -13,7 +14,7 @@
 
     <div>
         <x-input-label for="salario" :value="__('Salario Mensual')" />
-        <select id="salario" name="salario" class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm w-full">
+        <select id="salario" wire:model="salario" class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm w-full">
             <option>-- Seleccione --</option>
             @foreach ($salarios as $salario )
                 <option value="{{ $salario->id}}"> {{ $salario->salario}}</option>
@@ -23,7 +24,7 @@
 
     <div>
         <x-input-label for="categoria" :value="__('Categória')" />
-        <select id="categoria" name="categoria" class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm w-full">
+        <select id="categoria" wire:model="categoria" class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm w-full">
             <option>-- Seleccione --</option>
             @foreach ($categorias as $categoria )
                 <option value="{{ $categoria->id}}"> {{ $categoria->categoria}}</option>
@@ -35,7 +36,8 @@
         <x-input-label for="empresa" :value="__('Empresa')" />
         <x-text-input id="empresa"
             class="block mt-1 w-full"
-            type="text" name="empresa"
+            type="text"
+            wire:model="empresa"
             :value="old('empresa')"
             placeholder="Empresa: Ej. Netflix, uber, Spotify" />
         <x-input-error :messages="$errors->get('empresa')" class="mt-2" />
@@ -45,16 +47,17 @@
         <x-input-label for="ultimo_dia" :value="__('Último día para postularte')" />
         <x-text-input id="ultimo_dia"
             class="block mt-1 w-full"
-            type="date" name="ultimo_dia"
+            type="date"
+            wire:model="ultimo_dia"
             :value="old('ultimo_dia')"
             placeholder="Último día para postularte" />
         <x-input-error :messages="$errors->get('ultimo_dia')" class="mt-2" />
     </div>
 
     <div>
-        <x-input-label for="ultimo_dia" :value="__('Descripción Puesto')" />
+        <x-input-label for="descripcion" :value="__('Descripción Puesto')" />
         <textarea
-        name="descripcion"
+        wire:model="descripcion"
         id="descripcion"
         placeholder="Descripción general del puesto, experiencia"
         class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm w-full h-72"
@@ -67,7 +70,7 @@
         <x-text-input id="imagen"
             class="block mt-1 w-full"
             type="file"
-            name="imagen"
+            wire:model="imagen"
             
              />
         <x-input-error :messages="$errors->get('imagen')" class="mt-2" />
