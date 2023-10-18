318bb9a59c7a2d5efaf583f8fa7fe95d70a190bb
Javier Dominguez Galindo <joshjavy@gmail.com>
2023-10-18 14:19:10 -0600

    204. Problema con la migración de Vacantes

--- database/migrations/2023_10_17_204813_create__vacante_table.php
+++ database/migrations/2023_10_17_204813_create__vacante_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('vacantes', function (Blueprint $table) {
             $table->id();
+            $table->string('titulo');
+            $table->foreignId('salario_id')->constrained()->onDelete('cascade');
+            
             $table->timestamps();
         });
     }
