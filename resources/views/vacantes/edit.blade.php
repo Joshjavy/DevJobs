2cf127e563b597cc726abd4f75182daecd2c654a
Javier Dominguez Galindo <joshjavy@gmail.com>
2023-10-27 16:56:18 -0600

    216. El Método Mount de Livewire

--- resources/views/vacantes/edit.blade.php
+++ resources/views/vacantes/edit.blade.php
@@ -12,7 +12,9 @@
                     <h1 class="text-2   xl font-bold text-center mb-10">Editar Vacante {{ $vacante->titulo }}</h1>
                     <div class="md:flex md:justify-center p-5">
 
-                        <livewire:editar-vacante/>
+                        <livewire:editar-vacante
+                            :vacante="$vacante"
+                        />
                         
                     </div>
                 </div>
