4074e5b2de726d3d06e8e71e6f03e987545b3488
Javier Dominguez Galindo <joshjavy@gmail.com>
2023-11-05 16:11:15 -0600

    222. Añadiendo Sweet Alert 2

--- resources/views/livewire/mostrar-vacantes.blade.php
+++ resources/views/livewire/mostrar-vacantes.blade.php
@@ -1,43 +1,68 @@
-<div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-    @forelse ( $vacantes as $vacante)
-        <div class="p-1 bg-white border-b border-gray-200 md:flex md:justify-between md:items-center">
+<div>
+    <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+        @forelse ( $vacantes as $vacante)
+            <div class="p-1 bg-white border-b border-gray-200 md:flex md:justify-between md:items-center">
 
-            <div class=" leading-10">
-                <a href="#" class="text-xl font-bold">
-                    {{ $vacante->titulo }}
-                </a>
-                <p class="text-sm text-gray-600 font-bold"> {{ $vacante->empresa}}</p>
-                <p class="text-sm text-gray-500"> Último día: {{ $vacante->ultimo_dia->format('d/m/Y')}}</p>
-            </div>
+                <div class=" leading-10">
+                    <a href="#" class="text-xl font-bold">
+                        {{ $vacante->titulo }}
+                    </a>
+                    <p class="text-sm text-gray-600 font-bold"> {{ $vacante->empresa}}</p>
+                    <p class="text-sm text-gray-500"> Último día: {{ $vacante->ultimo_dia->format('d/m/Y')}}</p>
+                </div>
 
-            <div class=" flex flex-col md:flex-row items-strech gap-3  mt-5 md:mt-0">
-                <a href="#"
-                class=" bg-slate-800 py-2 px-4 rounded-lg text-white text-xs font-bold uppercase text-center">
-                    Candidatos
-                </a>
+                <div class=" flex flex-col md:flex-row items-strech gap-3  mt-5 md:mt-0">
+                    <a href="#"
+                    class=" bg-slate-800 py-2 px-4 rounded-lg text-white text-xs font-bold uppercase text-center">
+                        Candidatos
+                    </a>
 
-                <a href="{{ route('vacantes.edit',$vacante->id)}}"
-                class=" bg-blue-800 py-2 px-4 rounded-lg text-white text-xs font-bold uppercase text-center">
-                    Editar
-                </a>
-
-
-                <a href="#"
-                class=" bg-red-600 py-2 px-4 rounded-lg text-white text-xs font-bold uppercase text-center">
-                    Eliminar
-                </a>
-
-            </div>
+                    <a href="{{ route('vacantes.edit',$vacante->id)}}"
+                    class=" bg-blue-800 py-2 px-4 rounded-lg text-white text-xs font-bold uppercase text-center">
+                        Editar
+                    </a>
 
 
-        </div>    
-    @empty 
-    <!--212. Finalizando el Listado de Vacantes!-->
-    <div class="p-6 text-gray-900 dark:text-gray-100">
-        No hay vacantes a mostrar
+                    <a href="#"
+                    class=" bg-red-600 py-2 px-4 rounded-lg text-white text-xs font-bold uppercase text-center">
+                        Eliminar
+                    </a>
+
+                </div>
+
+
+            </div>    
+        @empty 
+        <!--212. Finalizando el Listado de Vacantes!-->
+        <div class="p-6 text-gray-900 dark:text-gray-100">
+            No hay vacantes a mostrar
+        </div>
+        @endforelse
     </div>
-    @endforelse
+    <div class=" mt-10">
+    {{ $vacantes->links() }}
+    </div>
 </div>
-<div class=" mt-10">
-{{ $vacantes->links() }}
-</div>
+@push('scripts')
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script>
+        Swal.fire({
+            title: '¿Eliminar Vacante?',
+            text: "Una vacante eliminada no se puede recuperar",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Si,¡ eliminar!'
+            }).then((result) => {
+            if (result.isConfirmed) {
+                Swal.fire(
+                'Deleted!',
+                'Your file has been deleted.',
+                'success'
+                )
+            }
+        })
+
+    </script>
+@endpush
