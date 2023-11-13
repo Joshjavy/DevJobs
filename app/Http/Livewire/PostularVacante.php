ea1f5c0c4f7b4b89deac109a91ce703eca58e71b
Javier Dominguez Galindo <joshjavy@gmail.com>
2023-11-12 19:05:01 -0600

    231. Validación del Formulario

--- app/Http/Livewire/PostularVacante.php
+++ app/Http/Livewire/PostularVacante.php
@@ -6,6 +6,26 @@ use Livewire\Component;
 
 class PostularVacante extends Component
 {
+
+    public $cv;
+    protected $rules=[
+        'cv'=>'required|mines:pdf'
+    ];
+
+    public function postularme()
+    {
+        $this->validate();
+
+        //Almacenar Cv en el HD
+
+        //Crear la vancante
+
+        //Crear notificación y enviar el email
+
+        //Mostrar al usuaru
+
+
+    }
     public function render()
     {
         return view('livewire.postular-vacante');
--- resources/views/livewire/postular-vacante.blade.php
+++ resources/views/livewire/postular-vacante.blade.php
@@ -1,13 +1,17 @@
 <div class="bg-gray-200 p-5 mt-5 flex flex-col justify-center items-center ">
     <h3 class="text-center text-2xl fond-bold my-4">Postularme a esta vacante</h3>
-    <form class="w-96 mt-5">
+    <form class="w-96 mt-5" wire:submit.prevent='postularme'>
         <div class="mb-4">
             <x-input-label for="vc" :value="__('Curriculum o Hoja de vida (PDF)')" />
             <x-text-input id="cv" type="file" 
                 accept=".pdf"
+                wire:model="cv"
                 class="block mt-1 w-full"
                 />
         </div>
+        @error('cv')
+            <livewire:mostrar-alerta :message="$message" />
+        @enderror
         <x-primary-button class=" w-full justify-center mt-5">
             {{ __('Postularme') }}
         </x-primary-button>    
