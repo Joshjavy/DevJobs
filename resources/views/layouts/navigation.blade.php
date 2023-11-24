31e91e29a2b5de017dde02f403d4dd9b9b4a89b6
Javier Dominguez Galindo <joshjavy@gmail.com>
2023-11-24 16:00:49 -0600

    244. Ocultando Notificaciones para Devs y Evitando que accedan a notificaciones

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -11,15 +11,17 @@
                 </div>
 
                 @auth
+                @can('create', App\Models\Vacante::class)
                     <!-- Navigation Links -->
-                    <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                        <x-nav-link :href="route('vacantes.index')" :active="request()->routeIs('vacantes.index')">
-                            {{ __('Mi vacantes') }}
-                        </x-nav-link>
-                        <x-nav-link :href="route('vacantes.create')" :active="request()->routeIs('vacantes.create')">
-                            {{ __('Crear Vacante') }}
-                        </x-nav-link>
-                    </div>
+                        <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                            <x-nav-link :href="route('vacantes.index')" :active="request()->routeIs('vacantes.index')">
+                                {{ __('Mi vacantes') }}
+                            </x-nav-link>
+                            <x-nav-link :href="route('vacantes.create')" :active="request()->routeIs('vacantes.create')">
+                                {{ __('Crear Vacante') }}
+                            </x-nav-link>
+                        </div>
+                    @endcan
                 @endauth
 
                 
@@ -28,13 +30,14 @@
             <!-- Settings Dropdown -->
             <div class="hidden sm:flex sm:items-center sm:ml-6">
                 @auth
-                    @if (auth()->user()->rol === 2)
+                    {{-- @if (auth()->user()->rol === 2) --}}
+                    @can('create', App\Models\Vacante::class)
                         <a href="{{ route('notificaciones')}}"
                         class="mr-2 w-7 h-7 bg-indigo-600 hover:bg-indigo-800 rounded-full
                          flex flex-col justify-center items-center text-sm text-white">
                             {{ Auth::user()->unreadNotifications->count() }}
                         </a>
-                    @endif
+                    @endcan
                     <x-dropdown align="right" width="48">
                         <x-slot name="trigger">
                             <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
