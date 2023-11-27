60873ab2adf8e88896db2ef39e1d563d3095f16b
Javier Dominguez Galindo <joshjavy@gmail.com>
2023-11-26 19:58:08 -0600

    250. Leyendo los datos ingresados en el Formulario

--- app/Http/Livewire/HomeVacantes.php
+++ app/Http/Livewire/HomeVacantes.php
@@ -6,7 +6,22 @@ use App\Models\Vacante;
 use Livewire\Component;
 
 class HomeVacantes extends Component
-{
+{  
+    public $termino;
+    public $categoria;
+    public $salario;
+
+    protected $listeners = ['terminosBusqueda'=>'buscar'];
+
+    public function buscar($termino, $categoria, $salario)
+    {
+        
+        $this->termino=$termino;
+        $this->categoria=$categoria;
+        $this->salario=$salario;
+
+
+    }
     public function render()
     {
         $vacantes=Vacante::all();
